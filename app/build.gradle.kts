d295c2d781b248795a4a88b9854398e0404f27cb
jeetsimanto <tanjiro.otsutsuki.spy@gmail.com>
2026-08-15 10:21:12 +0600

    UI: Remove redundant bottom selected day card from MemberMealCalendarView (v1.4.7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.messmanager.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 41
-        versionName = "1.4.6"
+        versionCode = 42
+        versionName = "1.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
